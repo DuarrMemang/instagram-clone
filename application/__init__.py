2327ae0bc4cc3a4d97629b1456c0ed6b79906a04
noxca <iamdynamical55@gmail.com>
2023-10-13 14:10:28 +0700

    finished building application

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask_session import Session
+# from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from dotenv import load_dotenv
+from datetime import timedelta
 import os
 
 load_dotenv()
@@ -12,9 +14,14 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
-Session(app)
+# Session(app)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from application.models import *
 from application import routes
